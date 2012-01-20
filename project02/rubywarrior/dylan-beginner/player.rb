2eebb141c849ece7946ade195597e4e104dbfda9
Dylan Denning <ddenning@mines.edu>
2012-01-19 22:36:59 -0700

    Beat level 3

--- project02/rubywarrior/dylan-beginner/player.rb
+++ project02/rubywarrior/dylan-beginner/player.rb
@@ -1,5 +1,13 @@
 class Player
   def play_turn(warrior)
-    warrior.walk!
+		if warrior.feel.empty?
+			if warrior.health < 19
+				warrior.rest!
+			else
+				warrior.walk!
+			end
+		else
+	    warrior.attack!
+		end
   end
 end
