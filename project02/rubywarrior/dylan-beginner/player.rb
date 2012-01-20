6ed25ad825e9a565faa8ce2a7a0c9c267fd52084
Dylan Denning <ddenning@mines.edu>
2012-01-19 22:52:56 -0700

    Beat level 5

--- project02/rubywarrior/dylan-beginner/player.rb
+++ project02/rubywarrior/dylan-beginner/player.rb
@@ -1,13 +1,29 @@
 class Player
   def play_turn(warrior)
-		if warrior.feel.empty?
-			if warrior.health < 19
-				warrior.rest!
-			else
-				warrior.walk!
-			end
-		else
-	    warrior.attack!
+		if @health == nil
+			@health = 20
 		end
+		if warrior.feel.captive?
+			warrior.rescue!
+		else
+			if warrior.health < @health
+				if warrior.feel.empty?
+					warrior.walk!
+				else
+					warrior.attack!
+				end
+			else
+				if warrior.feel.empty?
+					if warrior.health < 19
+						warrior.rest!
+					else
+						warrior.walk!
+					end
+				else
+					warrior.attack!
+				end
+			end
+		end
+		@health = warrior.health
   end
 end
