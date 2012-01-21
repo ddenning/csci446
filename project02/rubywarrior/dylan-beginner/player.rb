0ef5f1da1fb4102ef4e30c1956776337f9111bcc
Dylan Denning <ddenning@mines.edu>
2012-01-21 16:03:09 -0700

    Finished Level 7

--- project02/rubywarrior/dylan-beginner/player.rb
+++ project02/rubywarrior/dylan-beginner/player.rb
@@ -4,81 +4,21 @@ class Player
 		if @health == nil
 			@health = 20
 		end
-		if @been_backward == nil
-			if warrior.feel(:backward).captive?
-				warrior.rescue!(:backward)
-			else 
-				if warrior.health < @health
-					if warrior.feel(:backward).empty?
-						warrior.walk!(:backward)
-					else
-						warrior.attack!(:backward)
-					end
-				else
-					if warrior.feel(:backward).empty?
-						warrior.walk!(:backward)
-					else
-						@been_backward = true;
-					end
-				end
-			end
-		end
-		if @been_backward != nil
-			if warrior.feel.captive?
-				warrior.rescue!
-			else
-				if warrior.health < @health
-					if warrior.health < 10 and warrior.feel(:backward).empty?
-						warrior.walk!(:backward)
-					else
-						if warrior.feel.empty?
-							warrior.walk!
-						else
-							warrior.attack!
-						end
-					end
-				else
-					if warrior.feel.empty?
-						if warrior.health < 20
-							warrior.rest!
-						else
-							warrior.walk!
-						end
-					else
-						warrior.attack!
-					end
-				end
-			end
+		if @pivot == nil
+			warrior.pivot!
+			@pivot = true
+		else
+			act(warrior)
 		end
 		@health = warrior.health
   end
-	
-	def doBackwardStuff()
-		if warrior.feel(:backward).captive?
-			warrior.rescue!(:backward)
-		else 
-			if warrior.health < @health
-				if warrior.feel(:backward).empty?
-					warrior.walk!(:backward)
-				else
-					warrior.attack!(:backward)
-				end
-			else
-				if warrior.feel(:backward).empty?
-					warrior.walk!(:backward)
-				else
-					@been_backward = true;
-				end
-			end
-		end
-	end
 
-	def doForwardStuff()
+	def act(warrior)
 		if warrior.feel.captive?
-				warrior.rescue!
+			warrior.rescue!
 		else
 			if warrior.health < @health
-				if warrior.feel(:backward).empty?
+				if warrior.health < 10 and warrior.feel(:backward).empty?
 					warrior.walk!(:backward)
 				else
 					if warrior.feel.empty?
@@ -89,7 +29,7 @@ class Player
 				end
 			else
 				if warrior.feel.empty?
-					if warrior.health < 20
+					if warrior.health < 19
 						warrior.rest!
 					else
 						warrior.walk!
