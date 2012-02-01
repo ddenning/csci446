bbaaa3119f58d64acfcbc2f53fd4b0bcf8353067
Dylan Denning <ddenning@mines.edu>
2012-01-31 21:56:54 -0700

    Finished Iteration E1: Creating a Smarter Cart

--- project03/depot/app/controllers/line_items_controller.rb
+++ project03/depot/app/controllers/line_items_controller.rb
@@ -40,10 +40,9 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-  	
   	@cart = current_cart
   	product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
--- project03/depot/db/schema.rb
+++ project03/depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120201033525) do
+ActiveRecord::Schema.define(:version => 20120201044332) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120201033525) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
