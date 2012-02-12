0adf642441ca6623bbe59a2449af2622eb16cee0
Dylan Denning <ddenning@mines.edu>
2012-02-11 22:24:38 -0700

    Initial upload

---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '7deebe6f94503665b5a347a3cc121f1570dde2081f094a95d395bd5577bf8d9e4d3022e479dc945fae2a839c643febad985ff18843c843d8547d25b59d829952'
