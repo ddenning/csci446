383e92e4dfa703dda2d189aff0f3087db3f8b4e7
Dylan Denning <ddenning@mines.edu>
2012-02-14 14:22:32 -0700

    Why does it matter if we have our app inside a folder inside our project03 folder instead of having it directly inside our project03 folder? It's a lot easier to go back later and recognize it if we have a semantically meaningful name, not just project##.

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1f257ab25c781c634ad2800fe6a34a6ca944949d727cf8694daf262f87cb94440148c8af6ec372acb31767aec5fd2de08b2fcb0b90bf2dd0750a6dfdd3ba5b52'
