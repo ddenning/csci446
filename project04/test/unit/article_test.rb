2a877b8df7cc7228b7d9621e06c2b09b6faa2390
Dylan Denning <ddenning@mines.edu>
2012-02-14 14:45:18 -0700

    Changed project04/articles into project04

--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -4,7 +4,7 @@ class ArticleTest < ActiveSupport::TestCase
   test "num_edits must be 0 upon creation and must increment upon update" do
     article = Article.new
     assert article.num_edits = 0
-    article.update
+    Article.update :article, :title => "Hello"
     assert article.num_edits = 1
   end
 end
