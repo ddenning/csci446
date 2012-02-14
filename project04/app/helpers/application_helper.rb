205a6fb911dc31918254709307dee582b6bed4de
Dylan Denning <ddenning@mines.edu>
2012-02-14 16:33:50 -0700

    Fixed subtitles and sorted articles by created_at as per Yong's responses on Piazza.

--- project04/app/helpers/application_helper.rb
+++ project04/app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-	def generate_subtitle
-		subtitles = [
-  	"... with a name .com hipsters love!",
-  	"... what's \"Articl\" in Spanish anyways",
-  	"... my cousin works for Bit.ly",
-  	"... not just for the country of Spain"
-  ]
-  	subtitles.sample
-	end
 end
