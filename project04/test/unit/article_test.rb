d08ba5ba9b814af28de653ea372775feb57e8387
Dylan Denning <ddenning@mines.edu>
2012-02-19 13:56:02 -0700

    Unit tests for num_edits creation and update created and passing.

--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "num_edits must be 0 upon creation and must increment upon update" do
-    article = Article.new
+  test "num_edits must be 0 upon creation" do
+    article = new_article
     assert article.num_edits = 0
-    Article.update :article, :title => "Hello"
-    assert article.num_edits = 1
+  end
+
+  test "num_edits must increment upon update" do
+  	article = new_article
+  	article.save
+  	assert article.num_edits = 1
+  	article.save
+  	assert article.num_edits = 2
+  end
+
+  def new_article
+  	Article.new(title: "title",
+  							author: "author",
+  							body: "body")
   end
 end
