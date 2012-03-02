fe00288473a42388e8e7e859db02e0275f310a26
Dylan Denning <ddenning@mines.edu>
2012-03-01 21:34:18 -0700

    Pagination finished.

--- project05/app/controllers/articles_controller.rb
+++ project05/app/controllers/articles_controller.rb
@@ -2,7 +2,8 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    #@articles = Article.all
+    @articles = Article.paginate page: params[:page], order: 'created_at desc', per_page: 10
 
     respond_to do |format|
       format.html # index.html.erb
