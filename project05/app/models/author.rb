658e26f512007f7ef7472881c9f534da3b0dd239
Dylan Denning <ddenning@mines.edu>
2012-03-02 17:21:14 -0700

    Validates photo. Photo must be present. Photo must not be greater than 3MB in size. Photo must be .jpg or .png.

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ActiveRecord::Base
 	belongs_to :article
 
 	has_attached_file :photo, :styles => { :portrait => "500x300>" }
+
+	validates_attachment_presence :photo
+	validates_attachment_size :photo, :less_than => 3.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 end
