0687398890d6fc57f0150c6cc6740686287c44c5
Dylan Denning <ddenning@mines.edu>
2012-03-23 18:25:22 -0600

    Initial Upload

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '229ffa2e9b16a51b05f8b090548baa97dcd9e9b27b2cc47443e5ee88a271966de35620b1db3e827097cd5a57ad8fc11ca464c125914f606e096c168458555957'
