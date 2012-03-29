504d403347733b6a61c41c1632b0773761c4d2df
Dylan Denning <ddenning@mines.edu>
2012-03-29 00:08:41 -0600

    Initial users/_form.html.haml page created and flash upon successful creation looks good.

--- project06/app/controllers/users_controller.rb
+++ project06/app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: "Welcome, #{@user.first_name} #{@user.last_name}." }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
