176c825b534cbb71140fba7ee03f82c7745e397d
Dylan Denning <ddenning@mines.edu>
2012-03-30 11:48:11 -0600

    Initial upload of redone project06. I'm a little farther on here than where I left off, since logging in works and I don't get that stupid error about not having a destroy action when I actually do.

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '740365886d93286fc1e71c5014b837db5c7678e271a5e9b9bb036af3394027eadb855912afbb3c79778179d0c4cbf9123649597c14be5f7123169528813f0ea6'
