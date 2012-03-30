638d57f55b43f6a0c5277e1132840d8fa322b548
Dylan Denning <ddenning@mines.edu>
2012-03-30 13:33:05 -0600

    Added require_user and require_no_user helper methods and call them with a before filter for login/logout pages. Also added to user#index a way to display flash notices.

--- project06/app/controllers/user_sessions_controller.rb
+++ project06/app/controllers/user_sessions_controller.rb
@@ -1,5 +1,7 @@
 class UserSessionsController < ApplicationController
-  
+  before_filter :require_no_user, :only => [:new, :create]
+  before_filter :require_user, :only => [:destroy]
+
   def new
     @user_session = UserSession.new
   end
@@ -7,7 +9,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login_successful"
+      flash[:notice] = "Login successful"
       redirect_to root_url
     else
       render :action => :new
