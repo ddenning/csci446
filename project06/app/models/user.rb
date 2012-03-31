aab5fe5222245c09e69eb1d4ac115e0442418d1d
Dylan Denning <ddenning@mines.edu>
2012-03-30 19:45:19 -0600

    Added photo to user through paperclip gem.

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	acts_as_authentic
+
+	has_attached_file :photo, :styles => { :portrait => "300x500>" }
 end
--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120330163127) do
+ActiveRecord::Schema.define(:version => 20120331013403) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(:version => 20120330163127) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
